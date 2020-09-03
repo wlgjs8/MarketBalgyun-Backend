0c34c8e41e5991b9b516d49a5c915296c6f2dc09
Jeeheon Kim <wlgjs8@naver.com>
2020-09-03 14:35:22 +0900

    second commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -39,3 +41,16 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+// connect to mongoDB server
+mongoose.connect('mongodb://localhost:27017/test',{
+  useNewUrlParser:true,
+  useFindAndModify:false
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+  console.log('DB connected!');
+});
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+router.get('/', (req, res) => {
+  return res.json(users);
+})
+
+router.get('/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if(!id){
+    return res.status(400).json({err: 'Incorrect id'});
+  }
+
+  let user = users.filter(user => user.id == id)[0]
+  if(!user){
+    return res.status(404).json({err: 'Unknown user'});
+  }
+  return res.json(user);
+})
+
+
+let users = [
+  {
+    id: 1,
+    name: 'Hyun'
+  },
+  {
+    id: 2,
+    name: 'Alice'
+  },
+  {
+    id: 3,
+    name: 'Kelly'
+  }
+]
+
+module.exports = router;
+
+
+/*
+var express = require('express');
+var router = express.Router();
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+*/
