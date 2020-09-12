9b3ce2099f89fd0b552c2c7ed869d91990df8cf5
Jeeheon Kim <wlgjs8@naver.com>
2020-09-12 21:56:58 +0900

    20200912_0956

--- app.js
+++ app.js
@@ -1,56 +1,67 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const mongoose = require('mongoose');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+const passport = require("passport");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("connect-flash");
+require("dotenv").config();
+var connect = require("./model");
 // const bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const authRouter = require("./routes/auth");
+// const passportConfig = require("./passport");
 
 var app = express();
+connect();
+// passportConfig(passport);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser(cookieParser(process.env.COOKIE_SECRET)));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+app.use(flash());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
+// app.use(flash());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 module.exports = app;
-
-// connect to mongoDB server
-mongoose.connect('mongodb://localhost:27017/test',{
-  useNewUrlParser:true,
-  useFindAndModify:false
-});
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function(){
-  console.log('DB connected!');
-});
