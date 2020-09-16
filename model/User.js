945ffa42cb61a5419150809df409cef690c464af
Jeeheon Kim <wlgjs8@naver.com>
2020-09-16 15:47:21 +0900

    before merge

--- model/User.js
+++ model/User.js
@@ -1,27 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 const userSchema = new Schema({
-	ID:{
-		type:String,
-		required:true,
-		unique:true,
-		trim:true,
-	},
-	password:{
-		type:String,
-		required:true,
-		trim:true,
-	},
-	name:{
-		type:String,
-		required:true,
-		trim:true,
-	},
-	level:{
-		type:String,
-		requried:true,
-	}
+  ID: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  level: {
+    type: String,
+    requried: true,
+  },
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,22 @@
 var express = require("express");
 var router = express.Router();
+const User = require("../model/User");
 
-router.get("/", (req, res) => {
+router.use(express.json());
+
+router.get("/", async (req, res) => {
+  // const userTemp = await User.findOne({ ID: "0001" }[0]);
+  const userTemp = await User.find({ ID: "0001" });
+  userJson = JSON.stringify(userTemp);
+  console.log(userJson);
+
   return res.json(customerDB);
+});
+
+router.post("/", (req, res) => {
+  req.body.phone = "2222";
+  res.send(customerDB);
+  // console.log(m)
 });
 
 let customerDB = [
