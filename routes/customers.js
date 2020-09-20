2f02be5b2031098b9d7c66a72734596643ecb9ed
Jeeheon Kim <wlgjs8@naver.com>
2020-09-20 19:14:00 +0900

    Customers OK

--- routes/customers.js
+++ routes/customers.js
@@ -1,19 +1,15 @@
 var express = require("express");
 var router = express.Router();
 const Customer = require("../models/Customer");
-
 router.use(express.json());
 
 router.get("/", async (req, res) => {
   try {
-    // req.query.postPhone = "01021492222";
-    // const postPhone = req.query.postPhone;
-
-    const userTemp = await Customer.find({ name: "손예진" });
+    const postPhone = req.query.postPhone;
+    const userTemp = await Customer.findOne({ phone: postPhone });
 
     userJson = JSON.stringify(userTemp);
     res.send(userJson);
-    console.log(userJson);
   } catch (error) {
     console.log(error);
     return next(error);
@@ -21,7 +17,6 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  //Write schema from req.body
   console.log("HELLO POST");
 });
 
