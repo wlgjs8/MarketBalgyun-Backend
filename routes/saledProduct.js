6682109059e069e388648435c68977fa170b849b
Jeeheon Kim <wlgjs8@naver.com>
2020-09-27 02:20:08 +0900

    아오

--- routes/saledProduct.js
+++ routes/saledProduct.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 const GeneralProduct = require("../models/products/GeneralProduct");
 const ConsignProduct = require("../models/products/ConsignProduct");
+const Customer = require("../models/Customer");
 
 router.use(express.json());
 
@@ -15,7 +16,7 @@ router.post("/", async (req, res) => {
         if (generalProductTemp.length != 0) {
             await GeneralProduct.updateOne(
                 { id: req.body.id },
-                { $inc: { quantity: -1 } },
+                { $inc: { quantity: -req.body.quantity } },
             )
         } else {
             const consignProductTemp = await ConsignProduct.find({
@@ -24,24 +25,46 @@ router.post("/", async (req, res) => {
             if (consignProductTemp.length != 0) {
                 await ConsignProduct.updateOne(
                     { id: req.body.id },
-                    { $inc: { quantity: -1 } },
-                )
+                    { $inc: { quantity: -req.body.quantity } },
+                );
+                // 위탁자 포인트 적립
+                if (!req.body.accountable) {
+                    var pointPlus = req.body.price * 0.65;
+                    Customer.updateOne(
+                        { name: req.body.consigner },
+                        { $inc: { point: pointPlus } },
+                        function (err, res) {
+                            if (err) throw err;
+                        }
+                    )
+                }
+                else {
+                    const ConsingerTemp = Customer.find(
+                        { phone: req.body.phone }
+                    );
+                    var consignerJson = JSON.stringify(ConsingerTemp);
+                    res.send(consignerJson);
+                }
             }
             else {
                 res.send("해당 ID의 상품이 없습니다.");
             }
         }
-        // 포인트 적립 혹은 계좌 정보
+        // 구매자 포인트 적립
+        var pointPlus = req.body.cash + req.body.card;
+        pointPlus *= 0.65;
+        Customer.updateOne(
+            { name: req.body.name },
+            { $inc: { point: pointPlus } },
+            function (err, res) {
+                if (err) throw err;
+            }
+        )
         res.send("해당 ID의 상품이 판매되었습니다.");
     } catch (error) {
         console.log(error);
         return next(error);
     }
 });
-
-function setPoint(price) {
-    var pointPlus = price * 0.65;
-
-}
 
 module.exports = router;
