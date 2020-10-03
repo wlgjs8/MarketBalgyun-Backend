894a320e6d61c9f27b87692587ad46eb957a52d8
Jeeheon Kim <wlgjs8@naver.com>
2020-10-03 17:41:47 +0900

    흥

--- routes/showCSV.js
+++ routes/showCSV.js
@@ -5,7 +5,8 @@ const SaleLog = require("../models/saleLog");
 // const { post } = require("./generalCategory");
 const fs = require("fs");
 const moment = require("moment");
-const json2csv = require("json2csv").parse;
+// const json2csv = require("json2csv").parse;
+var xl = require("excel4node");
 const path = require("path");
 const fields = ["time", "first_category", "second_category", "third_category", "productName", "quantity", "single_price", "single_dicount",
     "single_apply_price", "card", "cash", "point", "total", "customer", "phone", "staff", "consigner", "bank", "account", "account_owner", "trader"];
@@ -13,50 +14,9 @@ const fields = ["time", "first_category", "second_category", "third_category", "
 
 //csv 출력 라우터
 router.get("/", async (req, res) => {
-    // 판매로그 csv
-    // start = req.query.start;
-    // end = req.query.end;
-    // duration_log = await SaleLog.find({ time: { "$gte": start, "$lte": end } }), select('-_id');
-    if (req.query.saleLog) {
-        // duration_log = await SaleLog.find().sort({ "_id": -1 });
+    if(req.query.saleLog){
 
-        // res.writeHead(200, {
-        //     'Content-Type': 'text/csv',
-        //     'Content-Disposition': 'attachment; filename=SaleLog.csv',
-        // });
-        // duration_log.csv(res);
-        SaleLog.find({}, function (err, salelogs) {
-            if (err) {
-                return res.status(500).json({ err });
-            }
-            else {
-                let csv
-                try {
-                    csv = json2csv(salelogs, { fields });
-                } catch (err) {
-                    return res.status(500).json({ err });
-                }
-                const dateTime = moment().format("YYYYMMDDhhmm");
-                const filePath = path.join(__dirname, "..", "public", "csv-" + dateTime + ".csv")
-                fs.writeFile(filePath, '\uFEFF' + csv, { encoding: 'utf8' }, function (err) {
-                    if (err) {
-                        return res.status(500).json({ err });
-                    }
-                    else {
-                        setTimeout(function () {
-                            fs.unlinkSync(filePath);
-                        }, 30000)
-                        res.setHeader(
-                            "Content-Disposition",
-                            "attachment; filename=" + "SaleLog.csv"
-                        );
-                        res.sendFile(filePath);
-                    }
-                })
-            }
-        })
     }
-
 });
 
 module.exports = router;
