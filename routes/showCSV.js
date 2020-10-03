8501ed73d8420df959494520405e0736fed956dd
Jihyun <bjih1999@naver.com>
2020-10-03 17:46:36 +0900

    excel4node 모듈 주석

--- routes/showCSV.js
+++ routes/showCSV.js
@@ -6,7 +6,7 @@ const SaleLog = require("../models/saleLog");
 const fs = require("fs");
 const moment = require("moment");
 // const json2csv = require("json2csv").parse;
-var xl = require("excel4node");
+//var xl = require("excel4node");
 const path = require("path");
 const fields = ["time", "first_category", "second_category", "third_category", "productName", "quantity", "single_price", "single_dicount",
     "single_apply_price", "card", "cash", "point", "total", "customer", "phone", "staff", "consigner", "bank", "account", "account_owner", "trader"];
