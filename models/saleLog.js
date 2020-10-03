3c97a8eadb6cff8b5542ee38eb7893504374debc
Jihyun <bjih1999@naver.com>
2020-10-03 15:45:16 +0900

    세일로그에 moment 추가

--- models/saleLog.js
+++ models/saleLog.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const mongoose_csv = require('mongoose-csv');
+const moment = require('moment');
 
 const saleLogSchema = new Schema({
 	time: {
