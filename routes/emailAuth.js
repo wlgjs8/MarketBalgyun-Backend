e99fb8752d87dfb965ec584542461f20985818f9
Jeeheon Kim <wlgjs8@naver.com>
2020-11-04 17:34:21 +0900

    https

--- routes/emailAuth.js
+++ routes/emailAuth.js
@@ -24,7 +24,7 @@ router.get("/verify", async (req, res) => {
     const tokenTemp = await Token.find({ token: req.query.id });
     const emailTemp = tokenTemp[0].email;
 
-    if ((req.protocol + "://" + req.get('host')) == ("http://" + tokenTemp[0].host)) {
+    if ((req.protocol + "://" + req.get('host')) == ("https://" + tokenTemp[0].host)) {
         if (tokenTemp[0].length != 0) {
             await Token.updateOne(
                 { email: emailTemp },
